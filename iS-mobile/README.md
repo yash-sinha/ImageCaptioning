a4e739c19b34d44ce15970073c2503dc37883318
Purnendu <purnendu@Purnendu.local>
2017-04-20 14:44:29 -0400

    adding iSpeak mobile app

---
+++ iS-mobile/README.md
@@ -0,0 +1,12 @@
+# City Savior
+
+Revamp of our project in Ionic. It is still in development.
+
+
+## Getting Started
+
+* Clone this repository.
+* Want to use TypeScript? Both the `master` branch and the `typescript` branch now use TypeScript.
+* Run `npm install` from the project root.
+* Install the ionic CLI (`npm install -g ionic`)
+* Run `ionic serve` in a terminal from the project root.
---
+++ iS-mobile/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+
+import { CitysavApp } from './app.component';
+import { IssuePostPage } from '../pages/post/post';
+
+
+import { PostDetailPage } from '../pages/postdetail/postdetail';
+
+
+
+@NgModule({
+  declarations: [
+    CitysavApp,
+    IssuePostPage,
+    PostDetailPage
+  ],
+  imports: [
+    IonicModule.forRoot(CitysavApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    CitysavApp,
+    IssuePostPage,
+    PostDetailPage
+  ],
+  providers: [ Storage]
+})
+export class AppModule { }
